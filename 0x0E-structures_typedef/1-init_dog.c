8c45e69f167bf5707672b30c1b9ab1c954fddf8a
yeabsira-asaminew <yeabsiraasaminew32@gmail.com>
2023-07-17 04:11:39 -0700

    1-init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,6 +13,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
 	d->name = name;
-	dog.age = age;
-	(*d).owner = owner;
+	d->age = age;
+	d->owner = owner;
 }
